6dc51e860c8239732f95cda18d1387d2f3ed45f1
Mohammad Shahadat Hossain <shahadatzcpe@users.noreply.github.com>
2016-11-21 02:49:36 +0600

    Update PhoneNumber.php
    There are five common mobile operator in bangaldesh their number prefix includeing country code Banglalink=+88019, grameenphone = 88017, Airtel=+88016, taletalk=+88015, robi=+88018;

--- src/Faker/Provider/bn_BD/PhoneNumber.php
+++ src/Faker/Provider/bn_BD/PhoneNumber.php
@@ -4,10 +4,12 @@ namespace Faker\Provider\bn_BD;
 
 class PhoneNumber extends \Faker\Provider\PhoneNumber
 {
+    static $operatorPrefix = ['+88019','+88017','+88016','+88015','+88018'];
+        
     public function phoneNumber()
     {
-        $number = "+880";
-        $number .= static::randomNumber(7);
+        $number = static::$operatorPrefix[rand(0, count(static::$operatorPrefix)-1)];
+        $number .= static::randomNumber(8);
 
         return Utils::getBanglaNumber($number);
     }
