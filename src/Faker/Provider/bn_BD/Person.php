525dcf98794bd8eaa48837ea160d47268e3ec263
Mohammad Shahadat Hossain <shahadatzcpe@users.noreply.github.com>
2016-11-21 02:39:49 +0600

    Update Person.php

--- src/Faker/Provider/bn_BD/Person.php
+++ src/Faker/Provider/bn_BD/Person.php
@@ -8,29 +8,41 @@ class Person extends \Faker\Provider\Person
         '{{firstNameMale}} {{lastName}}',
         '{{firstNameMale}} {{lastName}}',
         '{{firstNameMale}} {{lastName}}',
-        '{{titleMale}} {{firstNameMale}} {{lastName}}',
+        '{{titleMale}} {{firstNameMale}}',
+        '{{titleMale}} {{firstNameMale}} {{lastName}}',        
     );
 
     protected static $femaleNameFormats = array(
         '{{firstNameFemale}} {{lastName}}',
         '{{firstNameFemale}} {{lastName}}',
         '{{firstNameFemale}} {{lastName}}',
+        '{{titleFemale}} {{firstNameFemale}}',
         '{{titleFemale}} {{firstNameFemale}} {{lastName}}',
     );
 
     protected static $firstNameMale = array(
-        'অনন্ত', 'আব্দুল্লাহ', 'আহসান',  'ইমরুল', 'করিম', 'জলিল', 'বরকত', 'মাসনুন', 'রহিম',  'রিফাত', 'হাসনাত', 'হাসান',
+        'অনন্ত', 'আব্দুল্লাহ', 'আহসান',  'ইমরুল',
+        'করিম', 'জলিল', 'বরকত', 'মাসনুন',
+        'রহিম',  'রিফাত', 'হাসনাত', 'হাসান',
+        'শাহাদাত', 'আজাদ', 'সাইফুল', 'শাওন ',
+        'আজিজ', 'এমরান', 'পারভেজ', 'শোয়েব', 
+        'ইসমাইল', 'আনিস', 'মাসুম', 'এজাজুল', 
+        'ফিরোজ', 'জসিম', 'পলাশ', 'ফয়সাল', 
     );
 
     protected static $firstNameFemale = array(
-        'জারিন', 'জেরিন', 'ফারহানা', 'ফাহমেদা', 'মাহজাবিন', 'মেহনাজ', 'রহিমা', 'লাবনী', 'সাবরিন', 'সাবরিনা', 'হাসিন', 'রহমত',
+        'জারিন', 'জেরিন', 'ফারহানা', 'ফাহমেদা',
+        'মাহজাবিন', 'মেহনাজ', 'রহিমা', 'লাবনী',
+        'সাবরিন', 'সাবরিনা', 'হাসিন', 'রুমা', 
+        'তানভীন', 'তামান্না', 'লিমা', 'মলি',
+        'পলি','সানজিদা','সুমাইয়া','সেতু', 
     );
 
     protected static $lastName = array(
-        'খান', 'শেখ', 'শিকদার', 'আলী', 'তাসনীম', 'তাবাসসুম'
+        'খান', 'শেখ', 'শিকদার', 'আলী', 'তাসনীম', 'তাবাসসুম','হাওলাদার', 'মাতবর', 'কাজী' , 'মোল্লা', 'পাটুয়ারী
     );
 
-    protected static $titleMale = array('মি.');
+    protected static $titleMale = array('মি.', 'মোঃ' , 'কাজী', 'শেখ' );
 
-    protected static $titleFemale = array('মিসেস.', 'মিস.');
+    protected static $titleFemale = array('মিসেস.', 'মিস.', 'মোসাঃ' );
 }
